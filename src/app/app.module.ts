c52569110749c425038f0e8dc7e8acdc6d4becb1
vasyamakarchuk <vasyamakarchuk@outlook.com>
2017-09-23 03:13:22 +0300

    add login, signup, not-found modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,42 +4,47 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app.routing';
-import { ComponentsModule } from './components/components.module';
+import { AuthGuard } from './shared/guard/auth.guard';
+
+import { AppRoutingModule } from './app-routing.module';
+// import { ComponentsModule } from './components/components.module';
 
 import { AppComponent } from './app.component';
 
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
+// import { UserProfileComponent } from './user-profile/user-profile.component';
+// import { TableListComponent } from './table-list/table-list.component';
+// import { TypographyComponent } from './typography/typography.component';
+// import { IconsComponent } from './icons/icons.component';
+// import { MapsComponent } from './maps/maps.component';
+// import { NotificationsComponent } from './notifications/notifications.component';
+// import { UpgradeComponent } from './upgrade/upgrade.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    UserProfileComponent,
-    TableListComponent,
-    TypographyComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
-    UpgradeComponent,
-
+    // DashboardComponent,
+    // UserProfileComponent,
+    // TableListComponent,
+    // TypographyComponent,
+    // IconsComponent,
+    // MapsComponent,
+    // NotificationsComponent,
+    // UpgradeComponent,
+    // LoginComponent,
+    // NotFoundComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    ComponentsModule,
-RouterModule,
     AppRoutingModule
+    // ComponentsModule,
+    // RouterModule,
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
